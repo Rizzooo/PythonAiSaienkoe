f3df8ffdc9e64225f292429aceffef36096da553
c2222 <bladrizzo@gmail.com>
2025-03-29 16:28:42 +0200

    Lesson 7. Generator

--- Lesson7.py
+++ Lesson7.py
@@ -27,29 +27,62 @@
 #     print(elem)
 
 
-class Counter:
-    def __init__(self, max_number):
-        self.i = 0
-        self.max_number = max_number
+# class Counter:
+#     def __init__(self, max_number):
+#         self.i = 0
+#         self.max_number = max_number
+#
+#     def __iter__(self):
+#         self.i = 0
+#         return self
+#
+#     def __next__(self):
+#         self.i += 1
+#         if self.i > self.max_number:
+#             raise StopIteration
+#         return self.i
+#
+#
+# cont = Counter(5)
+# for elem in cont:
+#     print(elem)
+#
+# print("-"*30)
+# cont = Counter(5)
+# print(cont.__next__())
+# print(cont.__next__())
+# print(next(cont))
 
-    def __iter__(self):
-        self.i = 0
-        return self
-
-    def __next__(self):
-        self.i += 1
-        if self.i > self.max_number:
-            raise StopIteration
-        return self.i
+# # generator list
+# list_power_2 = [i ** 2 for i in range(1, 20, 1)]
+# print(list_power_2)
 
 
-cont = Counter(5)
-for elem in cont:
-    print(elem)
 
-print("-"*30)
-cont = Counter(5)
-print(cont.__next__())
-print(cont.__next__())
-print(next(cont))
 
+# def raise_to_the_degress(number):
+#     i = 0
+#     # for _ in range(max_degress):
+#     while True:
+#         result = number ** i
+#         yield result
+#         # i += 1
+#         if result > 100**20:
+#             return
+#         i+=1
+#
+# res = raise_to_the_degress(2)
+# print(res)
+#
+# for elem in res:
+#     print(elem)
+
+
+class Helper:
+    def __init__(self, work):
+        self.work = work
+    def __call__(self,work):
+        return f"I will help you with your {self.work}. Afterwarsd I will help you with {work}"
+
+hepler = Helper("homework")
+print(hepler('cleaning'))
