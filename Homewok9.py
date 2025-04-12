b4bf2f138f31d56fdf2fe5606d758627c0daa7f4
c2222 <bladrizzo@gmail.com>
2025-04-12 13:21:18 +0300

    HM9

---
+++ Homewok9.py
@@ -0,0 +1,34 @@
+import requests
+
+class CurrencyConverter:
+
+    def __init__(self):
+        self.rates = {}
+
+    def get_rates(self):
+        response = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+        data = response.json()
+        for item in data:
+            self.rates[item['cc']] = item['rate']
+
+    def convert(self, amount, from_currency, to_currency):
+        if from_currency != "USD":
+            amount = amount / self.rates[from_currency]
+        amount = round(amount * self.rates[to_currency], 2)
+        return amount
+
+converter = CurrencyConverter()
+converter.get_rates()
+
+while True:
+    try:
+        amount = float(input("Введіть суму валюти: "))
+        from_currency = input("Введіть код валюти суми, яку ви ввели: ")
+        to_currency = "USD"
+        converted_amount = converter.convert(amount, from_currency.upper(), to_currency)
+        print("Сума {} {} дорівнює {:.2f} USD".format(amount, from_currency.upper(), converted_amount))
+        break
+    except KeyError:
+        print("Ви ввели некоректний код валюти. Спробуйте ще раз.")
+    except ValueError:
+        print("Ви ввели некоректну суму. Спробуйте ще раз.")
